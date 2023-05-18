7b500a8d7e7452e1ba1ae154f51e94895ad0e250
unknown <gabrielrozas1996@gmail.com>
2023-05-18 13:25:17 -0800

    Fix error with sorting strings instead of numbers

--- src/modules/sorting.js
+++ src/modules/sorting.js
@@ -1,15 +1,18 @@
 const sorting = (arr) => {
   for (let i = 0; i < arr.length - 1; i += 1) {
-    let tempmax = arr[i].score;
+    let tempmax = Number(arr[i].score);
     let tempj = 0;
     for (let j = i + 1; j < arr.length; j += 1) {
-      if (tempmax < arr[j].score) {
-        tempmax = arr[j].score;
+      if (tempmax < Number(arr[j].score)) {
+        tempmax = Number(arr[j].score);
         tempj = j;
       }
     }
-    if (tempj > 0) [arr[i], arr[tempj]] = [arr[tempj], arr[i]];
+    if (tempj > 0) {
+      [arr[i], arr[tempj]] = [arr[tempj], arr[i]];
+    }
   }
+  return arr;
 };
 
 export default sorting;
